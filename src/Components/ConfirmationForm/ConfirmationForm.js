adebbd944d7bbf0a29a2bcb868a2c4b0885a4e99
Lucas Calabria <lucascalabria86@gmail.com>
2022-10-18 16:53:01 -0300

    Input Controlado

--- src/Components/ConfirmationForm/ConfirmationForm.js
+++ src/Components/ConfirmationForm/ConfirmationForm.js
@@ -1,11 +1,49 @@
 import React from 'react'
-import { Form} from '../MainPage/styles'
+import { Form, Input} from '../MainPage/styles'
 
-const ConfirmationForm = () => {
+const ConfirmationForm = (props) => {
+  function finalClick() {
+      alert("Parabéns você completou seu cadastro !")
+  }
   return (
     <Form>
-        
-      <button>Enviar dados</button>
+      <label>
+        Data de Nascimento:
+        <Input type="date" value={props.date} onChange={props.onChangeDate} />
+      </label>
+
+      <label>
+        Número do Celular
+        <Input type="number" placeholder="(00) 0000-0000" value={props.phone} onChange={props.onChangePhone} />
+      </label>
+
+      <label>
+        Você se considera de qual raça ?
+        <select onChange={props.onChangeRace} >
+          <option></option>
+          <option>Branco</option>
+          <option>Preto</option>
+          <option>Amarelo</option>
+          <option>Pardo</option>
+          <option>Indígena</option>
+          <option>Prefiro não dizer</option>
+        </select>
+      </label>
+
+      <label>
+        Você se considera de qual sexo ?
+        <select onChange={props.onChangeRace} >
+          <option></option>
+          <option>Homem cisgênero</option>
+          <option>Mulher cisgênero</option>
+          <option>Homem trans</option>
+          <option>Mulher trans</option>
+          <option>Pessoa não binária</option>
+          <option>Prefiro não dizer</option>
+        </select>
+      </label>
+
+      <button onClick={finalClick} >Enviar dados</button>
       </Form>
   )
 }
